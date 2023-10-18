7ae116fbc010f3109be750833c2227ec409f5fc6
HAOQUAN239 <146065167+HAOQUAN239@users.noreply.github.com>
2023-10-18 16:30:39 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+main()
+
+{
+
+int n;
+int c;
+
+scanf("%d",&n);
+
+for ( c=2 ; c < n  ; c++ )
+{
+if ( n%c == 0 )
+{
+printf("No");
+break;
+}
+
+}
+
+if ( c == n ){
+
+printf("Yes");
+
+
+
+}
+if(n<2)
+printf("No");
+return 0;
+}
 
