47e80511d270fe7395e75fadc552a0d5cda72c92
HAOQUAN239 <146065167+HAOQUAN239@users.noreply.github.com>
2023-10-18 16:31:15 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    int s1,d1,s2,d2,s3,d3;
+    scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+    int c=1;
+    if(d1>s2 || d2>s3){
+        c+=1;
+        printf("%d\n",c);
+    }
+    if(d1<=s2 || d2<=s3){
+        c=1;
+        printf("%d\n",c);
+    }
+
+
+}
 
