dba3b92617362002354c02c8408b713bbed9729e
HAOQUAN239 <146065167+HAOQUAN239@users.noreply.github.com>
2023-10-18 19:16:52 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -4,8 +4,39 @@
 int main()
 
 {
-    int s1,d1,s2,d2,s3,d3;
+    int s1,d1,s2,d2,s3,d3,temp;
     scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
+    if(s1>s2)
+    {
+     temp=s1;
+     s1=s2;
+     s2=temp;
+     temp=d1;
+     d1=d2;
+     d2=temp;
+
+    }
+    if(s1>s3)
+    {
+     temp=s1;
+     s1=s3;
+     s3=temp;
+     temp=d1;
+     d1=d3;
+     d3=temp;
+
+    }
+    if(s2>s3)
+    {
+     temp=s2;
+     s2=s3;
+     s3=temp;
+     temp=d2;
+     d2=d3;
+     d3=temp;
+
+    }
+
     int c=1;
     if(d1>s2){
         c+=1;
